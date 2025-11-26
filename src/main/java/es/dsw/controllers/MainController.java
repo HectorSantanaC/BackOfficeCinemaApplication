4daf9ef7a6599596ed3158041a89b4c3e5bd6eb8
Hector <hector.santana.gc@gmail.com>
2025-11-26 17:35:43 +0000

    Registrar último acceso del usuario mediante cookie

--- src/main/java/es/dsw/controllers/MainController.java
+++ src/main/java/es/dsw/controllers/MainController.java
@@ -1,18 +1,27 @@
 package es.dsw.controllers;
 
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import es.dsw.daos.UserDAO;
 import es.dsw.models.User;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class MainController {
 
 	@GetMapping(value = {"/", "/home"})
-	public String home(Model model, Authentication authentication) {
+	public String home(Model model, Authentication authentication, HttpServletResponse response) {
 		
 		// Username del usuario logueado
 		String username = authentication.getName();
@@ -23,11 +32,31 @@ public class MainController {
 		
 		model.addAttribute("user", user);
 		
+		// Fecha actual
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+		String fechaActual = LocalDateTime.now().format(formatter);
+		
+		// Codificar fechaActual para añadirla a la cookie
+		String fechaParaCookie = URLEncoder.encode(fechaActual, StandardCharsets.UTF_8);
+		
+		// Crear o modificar la cookie
+		Cookie cookie = new Cookie("cookie", String.valueOf(fechaParaCookie));;
+		cookie.setMaxAge(7 * 24 * 60 * 60);
+		
+		response.addCookie(cookie);
+		
 		return "home";
 	}
 	
 	@GetMapping(value = "/login")
-	public String login() {		
+	public String login(@CookieValue(name = "cookie", required = false) String lecturaCookie, Model model) {
+		
+		if (lecturaCookie != null) {
+	        // Decodificar la cookie
+	        String fechaDecodificada = URLDecoder.decode(lecturaCookie, StandardCharsets.UTF_8);
+	        model.addAttribute("ultimoAcceso", "Fecha del último acceso: " + fechaDecodificada);
+	    }
+		
 		return "login";
 	}
 }
